1364ee2b6ffc870391ee609aff5cfe30d935afd6
yaki <kiwidancebad@gmail.com>
2022-09-08 00:14:19 +0300

    cleanup App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import './App.css'
-import Triangulation from './animations/Triangulation'
 
 function App() {
-  return (
-    <div className="App">
-      <Triangulation />
-    </div>
-  )
+  return <div className="App"></div>
 }
 
 export default App
