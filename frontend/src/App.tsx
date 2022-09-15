0fa0ad76aa3f5f231325c34b9f1f6d125b5bc1c7
yaki <kiwidancebad@gmail.com>
2022-09-15 22:47:46 +0300

    cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import './App.css'
 
-import GamesApp from './test/components/GamesApp'
 import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <GamesApp />
-      </BrowserRouter>
+      <BrowserRouter></BrowserRouter>
     </div>
   )
 }
