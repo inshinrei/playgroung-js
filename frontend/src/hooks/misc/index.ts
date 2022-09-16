2b5a7ce38a0a6f64cc16d4dc017e237bfdf2b1ac
yaki <kiwidancebad@gmail.com>
2022-09-16 22:10:11 +0300

    create dom utils

--- frontend/src/hooks/misc/index.ts
+++ frontend/src/hooks/misc/index.ts
@@ -8,24 +8,6 @@ import {
   SetStateAction,
 } from 'react'
 
-export function on<T extends Window | Document | HTMLElement | EventTarget>(
-  obj: T | null,
-  ...args: Parameters<T['addEventListener']> | [string, Function | null, ...any]
-): void {
-  if (obj?.addEventListener) {
-    obj.addEventListener(...(args as Parameters<HTMLElement['addEventListener']>))
-  }
-}
-
-export function off<T extends Window | Document | HTMLElement | EventTarget>(
-  obj: T | null,
-  ...args: Parameters<T['removeEventListener']> | [string, Function | null, ...any]
-): void {
-  if (obj?.removeEventListener) {
-    obj.removeEventListener(...(args as Parameters<HTMLElement['removeEventListener']>))
-  }
-}
-
 export function useEffectOnce(effect: EffectCallback) {
   useEffect(effect, [])
 }
