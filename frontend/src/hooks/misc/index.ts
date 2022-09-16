0f5887c810351ba2c644f765f65445cb203a8b12
yaki <kiwidancebad@gmail.com>
2022-09-16 20:01:46 +0300

    utils

---
+++ frontend/src/hooks/misc/index.ts
@@ -0,0 +1,57 @@
+import {
+  useRef,
+  useState,
+  useEffect,
+  Dispatch,
+  useCallback,
+  EffectCallback,
+  SetStateAction,
+} from 'react'
+
+export function on<T extends Window | Document | HTMLElement | EventTarget>(
+  obj: T | null,
+  ...args: Parameters<T['addEventListener']> | [string, Function | null, ...any]
+): void {
+  if (obj?.addEventListener) {
+    obj.addEventListener(...(args as Parameters<HTMLElement['addEventListener']>))
+  }
+}
+
+export function off<T extends Window | Document | HTMLElement | EventTarget>(
+  obj: T | null,
+  ...args: Parameters<T['removeEventListener']> | [string, Function | null, ...any]
+): void {
+  if (obj?.removeEventListener) {
+    obj.removeEventListener(...(args as Parameters<HTMLElement['removeEventListener']>))
+  }
+}
+
+export function useEffectOnce(effect: EffectCallback) {
+  useEffect(effect, [])
+}
+
+export function useUnmount(fn: () => any): void {
+  const fnRef = useRef(fn)
+  fnRef.current = fn
+
+  useEffectOnce(() => () => fnRef.current())
+}
+
+export function useRefState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>] {
+  const frame = useRef(0)
+  const [state, setState] = useState(initialState)
+
+  const setRafState = useCallback((value: S | ((prevState: S) => S)) => {
+    cancelAnimationFrame(frame.current)
+
+    frame.current = requestAnimationFrame(() => {
+      setState(value)
+    })
+  }, [])
+
+  useUnmount(() => {
+    cancelAnimationFrame(frame.current)
+  })
+
+  return [state, setRafState]
+}
