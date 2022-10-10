edfb4e521c80affb00781aef8e133b8ed09475dc
yaki <kiwidancebad@gmail.com>
2022-10-10 19:15:53 +0300

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,11 @@ import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <BrowserRouter></BrowserRouter>
+    </div>
+  )
 }
 
 export default App
