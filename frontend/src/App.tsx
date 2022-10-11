fe421d050be6e799c36067eb403b748978dc217b
yaki <kiwidancebad@gmail.com>
2022-10-11 21:48:08 +0300

    a

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter></BrowserRouter>
+    </div>
+  )
+}
+
+export default App
