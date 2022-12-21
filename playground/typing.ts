1dffb015be7466b5f368af0ce09d429ab8f6afe2
yaki <kiwidancebad@gmail.com>
2022-12-21 12:47:43 +0300

    add prettier

--- playground/typing.ts
+++ playground/typing.ts
@@ -1,32 +1,33 @@
-type Reservation = unknown;
+type Reservation = unknown
 
 type Reserve = {
-  (from: Date, to: Date, destination: string): Reservation;
-  (from: Date, destination: string): Reservation;
-  (destination: string): Reservation;
-};
+  (from: Date, to: Date, destination: string): Reservation
+  (from: Date, destination: string): Reservation
+  (destination: string): Reservation
+}
 
 function call<T extends [unknown, string, ...unknown[]], R>(
   f: (...args: T) => R,
   ...args: T
 ): R {
-  return f(...args);
+  return f(...args)
 }
 
 function is<T>(a: T, ...b: [T, ...T[]]): boolean {
-  return b.every((_) => _ === a);
+  return b.every((_) => _ === a)
 }
 
 class RequestBuilder {
-  protected data: object | null = null;
-  protected method: "get" | "post" | null = null;
+  protected data: object | null = null
+  protected method: "get" | "post" | null = null
 
   setMethod(method: "get" | "post") {}
 }
 
-type None = unknown;
+type None = unknown
 
 interface Option<T> {
-  flatMap<U>(f: (value: T) => None): None;
-  flatMap<U>(f: (value: T) => Option<U>): Option<U>;
+  flatMap<U>(f: (value: T) => None): None
+  flatMap<U>(f: (value: T) => Option<U>): Option<U>
+  getOrElse(value: T): T
 }
