a9076daad4f793b7491b7c46ef6b689cfc595d05
yaki <kiwidancebad@gmail.com>
2023-01-31 13:08:44 +0300

    add keymaster interface

--- frontend/src/games/Tetris/src/keymaster.d.ts
+++ frontend/src/games/Tetris/src/keymaster.d.ts
@@ -9,3 +9,20 @@ interface KeymasterEvent {
 interface KeyHandler {
   (event: KeyboardEvent, keyEvent: KeymasterEvent): void
 }
+
+interface FilterEvent {
+  target?: { tagName?: string }
+  srcElement?: { tagName?: string }
+}
+
+interface Keymaster {
+  (key: string, callback: KeyHandler): void
+  (key: string, scope: string, callback: KeyHandler): void
+
+  shift: boolean
+  alt: boolean
+  option: boolean
+  ctrl: boolean
+  control: boolean
+  command: boolean
+}
