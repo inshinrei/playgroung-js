27ac909d90f9e77c483c8642cc8bb2e4916c2a8b
yaki <kiwidancebad@gmail.com>
2023-02-19 18:35:35 +0300

    add Reminder

---
+++ projects/LockingDashboard/Reminder/Reminder.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Reminder.module.scss'
+
+function Reminder() {
+  return (
+    <div className={styles.reminder}>
+      <span className={styles.reminderText}>Welcome</span>
+    </div>
+  )
+}
+
+export default Reminder
