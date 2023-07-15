e86bbb8b8373cdd0446eb583a1e600b966ff738d
tokkiumai <kiwidancebad@gmail.com>
2023-07-15 23:18:38 +0300

    nano1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
+import { atom } from 'nanostores'
+
+const $list = atom([])
+
+function addItem(item) {
+  $list.set([...$list.get(), item])
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-
-    </div>
-  )
+  return <div className="App"></div>
 }
 
 export default App
