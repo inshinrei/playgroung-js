12cbc71663932856bab2fc456b5b54e3a73250aa
tokkiumai <kiwidancebad@gmail.com>
2023-08-29 17:01:36 +0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Loader from './bin-but-not-binary/rct-ui-loader'
+
 import styles from './App.module.scss'
 
 function App() {
-  return (
-    <div className={styles.root}>
-      <Loader />
-    </div>
-  )
+  return <div className={styles.root}></div>
 }
 
 export default App
