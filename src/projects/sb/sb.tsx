c904da349e4d0985e1e269916f16219cb1d826d9
tokkiumai <kiwidancebad@gmail.com>
2024-02-28 21:38:24 +0300

    feat: thumb moving

--- src/projects/sb/sb.tsx
+++ src/projects/sb/sb.tsx
@@ -1,20 +1,7 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import styles from './sb.module.scss'
 import cn from 'classnames'
-
-interface BarProps {
-  className?: string
-}
-
-function Bar({ className }: BarProps) {
-  return (
-    <div className={cn(styles.barContainer, className)}>
-      <div className={styles.bar} />
-      <div className={styles.thumb} />
-    </div>
-  )
-}
 
 export function Sb() {
   return (
@@ -30,6 +17,32 @@ export function Sb() {
         <Bar className={styles.horizontal} />
         <Bar className={styles.horizontal} />
       </div>
+    </div>
+  )
+}
+
+interface BarProps {
+  className?: string
+}
+
+function Bar({ className }: BarProps) {
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    let interval: number
+    let e = ref.current
+    if (e) {
+      interval = setInterval(() => {
+        e!.style.transform = `translateY(${Math.random() * 100}px)`
+      }, 700)
+    }
+    return () => clearInterval(interval)
+  }, [ref.current])
+
+  return (
+    <div className={cn(styles.barContainer, className)}>
+      <div className={styles.bar} />
+      <div className={styles.thumb} ref={ref} />
     </div>
   )
 }
