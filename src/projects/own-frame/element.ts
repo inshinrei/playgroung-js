eed8fafe894c3baa44b1c3e43d510a19fe40eaf9
tokkiumai <kiwidancebad@gmail.com>
2025-02-21 21:49:29 +0300

    own react implementation: reconcile

--- src/projects/own-frame/element.ts
+++ src/projects/own-frame/element.ts
@@ -1,15 +1,15 @@
 export let TEXT_ELEMENT = 'TEXT_ELEMENT'
 
-export function createElement(type, config, ...args) {
+export function createElement(type: any, config: any, ...args: any[]) {
   let props = Object.assign({}, config)
   let hasChildren = args.length > 0
   let rawChildren = hasChildren ? [].concat(...args) : []
   props.children = rawChildren
-    .filter((c) => c !== null && c !== false)
-    .map((c) => (c instanceof Object ? c : createTextElement(c)))
+    .filter((c: any) => c !== null && c !== false)
+    .map((c: any) => (c instanceof Object ? c : createTextElement(c)))
   return { type, props }
 }
 
-function createTextElement(value) {
+function createTextElement(value: string) {
   return createElement(TEXT_ELEMENT, { nodeValue: value })
 }
--- src/projects/own-frame/reconciler.ts
+++ src/projects/own-frame/reconciler.ts
@@ -1,25 +1,48 @@
 import { TEXT_ELEMENT } from './element'
 
-export function render(element, container) {
+let rootInstance: unknown = null
+
+export function render(element: unknown, container: unknown) {
+  let prevInstance = rootInstance
+  let nextInstance = reconcile(container, prevInstance, element)
+  rootInstance = nextInstance
+}
+
+function reconcile(container: any, instance: any, element: unknown) {
+  if (instance === null) {
+    let newInstance = instantiate(element)
+    container.appendChild(newInstance.dom)
+    return newInstance
+  } else {
+    let newInstance = instantiate(element)
+    container.replaceChild(newInstance.dom, instance.dom)
+    return newInstance
+  }
+}
+
+function instantiate(element: any) {
   let { type, props } = element
   let isTextElement = type === TEXT_ELEMENT
   let dom = isTextElement
     ? document.createTextNode('')
     : document.createElement(type)
-  let isListener = (name) => name.startsWith('on')
+  let isListener = (name: string) => name.startsWith('on')
   Object.keys(props)
     .filter(isListener)
     .forEach((name) => {
       let eventType = name.toLowerCase().substring(2)
       dom.addEventListener(eventType, props[name])
     })
-  let isAttribute = (name) => !isListener(name) && name !== 'children'
+  let isAttribute = (name: string) => !isListener(name) && name !== 'children'
   Object.keys(props)
     .filter(isAttribute)
     .forEach((name) => {
       dom[name] = props[name]
     })
   let childElements = props.children ?? []
-  childElements.forEach((child) => render(child, dom))
-  container.appendChild(dom)
+  let childInstances = childElements.map(instantiate)
+  let childDoms = childInstances.map((instance: any) => instance.dom)
+  childDoms.forEach((dom: any) => dom.appendChild(dom))
+  const instance = { dom, element, childInstances }
+  return instance
 }
