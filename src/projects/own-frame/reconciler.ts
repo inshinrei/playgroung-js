3042a0f166521f8dcebcb95f920a54e7c3f9a09e
tokkiumai <kiwidancebad@gmail.com>
2025-02-21 23:55:09 +0300

    own react implementation: reconciliation logic

--- src/projects/own-frame/reconciler.ts
+++ src/projects/own-frame/reconciler.ts
@@ -8,16 +8,38 @@ export function render(element: unknown, container: unknown) {
   rootInstance = reconcile(container, prevInstance, element)
 }
 
-function reconcile(container: any, instance: any, element: unknown) {
+function reconcile(container: any, instance: any, element: any) {
   if (instance === null) {
     let newInstance = instantiate(element)
     container.appendChild(newInstance.dom)
     return newInstance
+  } else if (element === null) {
+    container.removeChild(instance.dom)
+    return null
+  } else if (instance.element.type === element.type) {
+    updateDomProperties(instance.dom, instance.element.props, element.props)
+    instance.childInstances = reconcileChildren(instance, element)
+    instance.element = element
+    return instance
   } else {
     let newInstance = instantiate(element)
     container.replaceChild(newInstance.dom, instance.dom)
     return newInstance
   }
+}
+
+function reconcileChildren(instance: any, element: any) {
+  let { dom, childInstances } = instance
+  let nextChildElements = element.props.children ?? []
+  let newChildInstances = []
+  let count = Math.max(childInstances.length, nextChildElements.length)
+  for (let i = 0; i < count; i++) {
+    let childInstance = childInstances[i]
+    let childElement = nextChildElements[i]
+    let newChildInstance = reconcile(dom, childInstance, childElement)
+    newChildInstances.push(newChildInstance)
+  }
+  return newChildInstances.filter((i) => i !== null)
 }
 
 function instantiate(element: any) {
