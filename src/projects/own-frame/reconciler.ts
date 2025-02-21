9a42236b24e384f953d0c3f7c9216d0abd70470f
tokkiumai <kiwidancebad@gmail.com>
2025-02-21 21:38:05 +0300

    own react implementation: reconciler

---
+++ src/projects/own-frame/reconciler.ts
@@ -0,0 +1,23 @@
+export function render(element, container) {
+  let { type, props } = element
+  let isTextElement = type === 'TEXT_ELEMENT'
+  let dom = isTextElement
+    ? document.createTextNode('')
+    : document.createElement(type)
+  let isListener = (name) => name.startsWith('on')
+  Object.keys(props)
+    .filter(isListener)
+    .forEach((name) => {
+      let eventType = name.toLowerCase().substring(2)
+      dom.addEventListener(eventType, props[name])
+    })
+  let isAttribute = (name) => !isListener(name) && name !== 'children'
+  Object.keys(props)
+    .filter(isAttribute)
+    .forEach((name) => {
+      dom[name] = props[name]
+    })
+  let childElements = props.children ?? []
+  childElements.forEach((child) => render(child, dom))
+  container.appendChild(dom)
+}
