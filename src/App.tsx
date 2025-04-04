3d6231dfeb90e2de0059ae4e1327d6a9d4d46f99
tokkiumai <kiwidancebad@gmail.com>
2025-04-04 20:20:52 +0300

    test: store

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 
 import styles from './App.module.scss'
-import { DsEvents } from './projects/dsevents'
+import { SomeComponent } from './bin-but-not-binary/noir-test/comp'
 
 function App() {
   return (
-    <div className={styles.root}>
-      <DsEvents />
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <div
+        className={styles.root}
+        style={{
+          width: 350,
+          padding: '20px',
+          height: 750,
+          border: '1px solid blue',
+        }}
+      >
+        <SomeComponent />
+      </div>
     </div>
   )
 }
---
+++ src/bin-but-not-binary/noir-test/store.ts
@@ -0,0 +1,116 @@
+import { action, makeObservable, observable, reaction } from 'mobx'
+
+const storage = {
+  get: (key) => {
+    return localStorage.getItem(key)
+  },
+  set: (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value))
+  },
+  delete: (key) => {
+    localStorage.deleteItem(key)
+  },
+}
+// want to be
+const config = {
+  key: 'string',
+  version: 0,
+  storage: storage,
+  // migration: createMigration(migrations),
+}
+
+const migrations = {
+  0: (store: Store) => {
+    return {}
+  },
+  1: (store: Store) => {},
+}
+
+class BaseModel {
+  storageWhitelist: Array<string> = []
+  private version: number
+  private key: string
+  private storage: Storage
+  private migration: Migration
+
+  constructor(config) {
+    this.version = config.version
+    this.key = config.key
+    this.storage = config.storage
+    this.migration = config.migration
+    this.storageWhitelist = config.whitelist
+    console.debug(
+      'init with config',
+      config,
+      'whitelist',
+      this.storageWhitelist,
+    )
+  }
+
+  onStart() {
+    // check if storage have changed, storage migration
+    // check the version, migration may be needed
+  }
+
+  // pass whitelist as arg
+  sub() {
+    this.storageWhitelist.forEach((item) => {
+      console.debug('whitelist item: ', item)
+      reaction(
+        () => this[item],
+        (d) => {
+          console.debug('reaction item: ', item, d)
+          this.storage.set(this.key + item, d)
+        },
+      )
+    })
+  }
+}
+
+export class Store extends BaseModel {
+  state1 = 1
+  state2 = 'string'
+
+  public storageWhitelist = ['state1', 'state2']
+
+  constructor() {
+    super({ ...config, whitelist: ['state1', 'state2'] })
+    makeObservable(this, {
+      state1: observable,
+      state2: observable,
+      setState1: action.bound,
+    })
+
+    this.sub()
+  }
+
+  setState1(v) {
+    console.debug('setState1', v)
+    this.state1 = v
+  }
+
+  setState2(v) {
+    this.state2 = v
+  }
+}
+
+export const store = new Store()
+
+interface MigrationFunc {
+  to: number
+  run: () => void | never
+}
+
+interface Migration {}
+
+interface Storage {
+  get: (key: string) => any
+  set: (key: string, value: any) => void
+  delete: (key: string) => void
+}
+
+interface StorageData {
+  data: any
+  version: number
+  storageKey: ''
+}
