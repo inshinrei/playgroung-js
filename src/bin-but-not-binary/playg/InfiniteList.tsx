753d323389226341df2e7cfce00fc1e402dd7b51
tokkiumai <kiwidancebad@gmail.com>
2025-04-07 20:39:58 +0300

    virtual scroll: helpers

--- src/bin-but-not-binary/playg/InfiniteList.tsx
+++ src/bin-but-not-binary/playg/InfiniteList.tsx
@@ -556,6 +556,176 @@ function usePrevNew<T extends any>(current: T) {
 }
 
 // module: iteratees
-function areSortedArraysEqual() {}
+function areSortedArraysEqual(array1: any[], array2: any[]) {
+  if (array1.length !== array2.length) {
+    return false
+  }
+  return array1.every((item, i) => item === array2[i])
+}
 
 // module: useInfiniteScroll
+type GetMore = (args: { direction: LoadMoreDirection }) => void
+type LoadMoreBackwards = (args: { offsetId?: number | string }) => void
+
+const DEFAULT_LIST_SLICE = 30
+
+function useInfiniteScroll<ListId extends string | number>(
+  loadMoreBackwards?: LoadMoreBackwards,
+  listIds?: ListId[],
+  isDisabled = false,
+  listSlice = DEFAULT_LIST_SLICE,
+): [ListId[]?, GetMore?, number?] {
+  let requestParamsRef = useRef<{
+    direction?: LoadMoreDirection
+    offsetId?: ListId
+  }>()
+  let currentStateRef = useRef<
+    { viewportIds: ListId[]; isOnTop: boolean; offset: number } | undefined
+  >()
+  if (!currentStateRef.current && listIds && !isDisabled) {
+    let { newViewportIds, newIsOnTop, fromOffset } = getViewportSlice(
+      listIds,
+      LoadMoreDirection.Forwards,
+      listSlice,
+      listIds[0],
+    )
+    currentStateRef.current = {
+      viewportIds: newViewportIds,
+      isOnTop: newIsOnTop,
+      offset: fromOffset,
+    }
+  }
+  let forceUpdate = useForceUpdate()
+  if (isDisabled) {
+    requestParamsRef.current = {}
+  }
+  let prevListIds = usePrevious(listIds)
+  let prevIsDisabled = usePrevious(isDisabled)
+  if (
+    listIds &&
+    !isDisabled &&
+    (listIds !== prevListIds || isDisabled !== prevIsDisabled)
+  ) {
+    let { viewportIds, isOnTop } = currentStateRef.current || {}
+    let currentMiddleId =
+      viewportIds && !isOnTop
+        ? viewportIds[Math.round(viewportIds.length / 2)]
+        : undefined
+    let defaultOffsetId =
+      currentMiddleId && listIds.includes(currentMiddleId)
+        ? currentMiddleId
+        : listIds[0]
+    let { offsetId = defaultOffsetId, direction = LoadMoreDirection.Forwards } =
+      requestParamsRef.current || {}
+    let { newViewportIds, newIsOnTop, fromOffset } = getViewportSlice(
+      listIds,
+      direction,
+      listSlice,
+      offsetId,
+    )
+    requestParamsRef.current = {}
+    if (!viewportIds || !areSortedArraysEqual(viewportIds, newViewportIds)) {
+      currentStateRef.current = {
+        viewportIds: newViewportIds,
+        isOnTop: newIsOnTop,
+        offset: fromOffset,
+      }
+    }
+  } else if (!listIds) {
+    currentStateRef.current = undefined
+  }
+
+  const getMore: GetMore = useLastCallback(
+    ({
+      direction,
+      noScroll,
+    }: {
+      direction: LoadMoreDirection
+      noScroll?: boolean
+    }) => {
+      let { viewportIds } = currentStateRef.current || {}
+      let offsetId = viewportIds
+        ? direction === LoadMoreDirection.Backwards
+          ? viewportIds[viewportIds.length - 1]
+          : viewportIds[0]
+        : undefined
+      if (!listIds) {
+        if (loadMoreBackwards) {
+          loadMoreBackwards({ offsetId })
+        }
+        return
+      }
+      let {
+        newViewportIds,
+        areSomeLocal,
+        areAllLocal,
+        newIsOnTop,
+        fromOffset,
+      } = getViewportSlice(listIds, direction, listSlice, offsetId)
+      if (
+        areSomeLocal &&
+        !(viewportIds && areSortedArraysEqual(viewportIds, newViewportIds))
+      ) {
+        currentStateRef.current = {
+          viewportIds: newViewportIds,
+          isOnTop: newIsOnTop,
+          offset: fromOffset,
+        }
+        forceUpdate()
+      }
+      if (!areAllLocal && loadMoreBackwards) {
+        if (!noScroll) {
+          requestParamsRef.current = {
+            ...requestParamsRef.current,
+            direction,
+            offsetId,
+          }
+        }
+        loadMoreBackwards({ offsetId })
+      }
+    },
+  )
+  return isDisabled
+    ? [listIds]
+    : [
+        currentStateRef.current?.viewportIds,
+        getMore,
+        currentStateRef.current?.offset,
+      ]
+}
+
+function getViewportSlice<ListId extends string | number>(
+  sourceIds: ListId[],
+  direction: LoadMoreDirection,
+  listSlice: number,
+  offsetId?: ListId,
+) {
+  let { length } = sourceIds
+  let index = offsetId ? sourceIds.indexOf(offsetId) : 0
+  let isForwards = direction === LoadMoreDirection.Forwards
+  let indexForDirection = isForwards ? index : index + 1 || length
+  let from = Math.max(0, indexForDirection - listSlice)
+  let to = indexForDirection + listSlice - 1
+  let newViewportIds = sourceIds.slice(Math.max(0, from), to + 1)
+  let areSomeLocal
+  let areAllLocal
+  switch (direction) {
+    case LoadMoreDirection.Forwards:
+      areSomeLocal = indexForDirection >= 0
+      areAllLocal = from >= 0
+      break
+    case LoadMoreDirection.Backwards:
+      areSomeLocal = indexForDirection < length
+      areAllLocal = to <= length - 1
+      break
+  }
+  return {
+    newViewportIds,
+    areSomeLocal,
+    areAllLocal,
+    newIsOnTop: newViewportIds[0] === sourceIds[0],
+    fromOffset: from,
+  }
+}
+
+//
