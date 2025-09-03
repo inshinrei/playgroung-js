f6041f2db9366fc016b8ac57f9687291c496ce11
tokkiumai <kiwidancebad@gmail.com>
2025-09-03 23:41:58 +0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -29,40 +29,6 @@ function App() {
 
     // halua.debug('second message', 'count', 2, 'somethingElse', 'aboba')
     // halua.debug('message', 'aaaa')
-
-    // let logger = halua.New([
-    //   NewWebConsoleHandler(self.console, {
-    //     pretty: true,
-    //   }),
-    // ])
-    // let logger2 = logger.With('count', 2)
-    // logger2.debug('logger 2 debug message')
-    // logger2.info('logger 2 info')
-    // logger2.warn('logger 2 warn')
-    // logger2.error('logger 2
-    //   .New(NewTextHandler(self.console.log))
-    //   .withMessageFormat('[some prefix] %t %l %a > %w')
-    // textLogger.info('infoooo')
-    // textLogger.With('aboba').info('infooo abobbba')
-    //
-    // let webLogger = halua.New(
-    //   NewWebConsoleHandler(self.console, {
-    //     messageFormat: '[aboba] %t - %l : %a',
-    //   }),
-    // )
-    // webLogger.info('keka')
-    // // console 100_000 4889.900000095367
-    // JSON 100_000    5929.099999904633
-    // web 100_000     3630.800000190735
-
-    //   'second message',
-    //   'count',
-    //   2,
-    //   'somethingElse',
-    //   'aboba',
-    //   'none',
-    // )
-    // logger2.info('info')
   })
 
   return (
