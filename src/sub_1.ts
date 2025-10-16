e1efae7b301ff3fadec47136d18ebbf5f0398e6e
tokkiumai <kiwidancebad@gmail.com>
2025-10-16 21:29:50 +0300

    generators tests

--- src/sub_1.ts
+++ src/sub_1.ts
@@ -1,23 +1,70 @@
-export function main() {
-  const obj = { prop: 'value', prop1: 'v1' }
-  let g = unpack(obj)
-  delete obj.prop
-  console.log(g.next().value)
-}
-
 function run(...args: any[]) {
-  let g = unpack(...args)
-  while (true) {
-    let r = g.next()
-    if (r.done) {
-      break
+    let g = unpack(...args)
+    while (true) {
+        let r = g.next()
+        if (r.done) {
+            break
+        }
+        console.log(r.value)
     }
-    console.log(r.value)
-  }
 }
 
 function* unpack(...args: any[]) {
-  for (let v of args) {
-    yield v
-  }
+    for (let v of args) {
+        yield v
+    }
+}
+
+class Halua {
+    constructor(private handlers: Array<any>) {}
+
+    info(...args: any[]) {
+        const gen = this.run(...args)
+        let v
+        while (!v?.done) {
+            v = gen.next()
+            console.log(this.handlers.map((h) => h.queue.size))
+        }
+        console.log('done')
+    }
+
+    *run(...args: any[]) {
+        for (let v of args) {
+            // while argsgen.next()
+            for (let h of this.handlers) {
+                // yield argument computation
+                yield h.appendArg(v)
+            }
+        }
+        for (let h of this.handlers) {
+            h.send()
+        }
+    }
+}
+
+class Handler {
+    queue = new Set()
+
+    some() {}
+
+    appendArg(arg: any) {
+        this.queue.add(arg)
+    }
+
+    send() {
+        let args = Array.from(this.queue)
+        this.queue.clear()
+        console.log(...args)
+    }
+}
+
+class Handler_1 extends Handler {}
+
+class Handler_2 extends Handler {}
+
+const halua = new Halua([new Handler_1(), new Handler_2()])
+
+export function main() {
+    halua.info('string', [1, 2, 3])
+    halua.info({ prop: 'value' }, 'hello!!!')
 }
