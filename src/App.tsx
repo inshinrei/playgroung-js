47d85b82733b2dfb77d55eeb3f2b3dfcf57133e0
tokkiumai <kiwidancebad@gmail.com>
2025-12-01 23:10:37 +0300

    generators tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { CastMain } from './projects/cast/main'
-import {
-  halua,
-  NewJSONHandler,
-  NewTextHandler,
-  NewWebConsoleHandler,
-} from '../../projects/inshinrei/halua/src'
-import { main } from './sub_1'
+import { halua, NewJSONHandler, NewTextHandler, NewWebConsoleHandler } from '../../projects/inshinrei/halua/src'
+import { Halua } from '../../projects/inshinrei/halua/src/main/halua'
 
 let logger = halua.New([
-  NewTextHandler(self.console.info),
-  NewWebConsoleHandler(self.console),
-  NewJSONHandler(self.console.info),
+    NewTextHandler(self.console.info),
+    NewWebConsoleHandler(self.console),
+    NewJSONHandler(self.console.info),
 ])
 
+let h = new Halua()
+
 function App() {
-  useEffect(() => {
-    main()
-  })
-
-  function run() {
-    let arr = Array.from({ length: 1000 }).fill({ value: 'propov' })
-    for (let v of arr) {
-      logger.info(v)
+    function run() {
+        let arr = Array.from({ length: 1000 }).fill({ value: 'propov' })
+        for (let v of arr) {
+            logger.info(v)
+        }
+        console.log('done', arr.length)
     }
-    console.log('done', arr.length)
-  }
 
-  return (
-    <div>
-      <CastMain />
-      <div onClick={main}>click</div>
-    </div>
-  )
+    return (
+        <div>
+            <CastMain />
+
+            <div
+                onClick={() => {
+                    h.info('string', 123, [1, 2, 3])
+                }}
+            >
+                log something
+            </div>
+        </div>
+    )
 }
 
 export default App
